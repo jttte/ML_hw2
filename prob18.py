66482812a0853937eeaddf9ebaa93fe9fbd6b6b5
Lucy Lin <Lucy@cardinalblue.com>
2013-10-20 10:48:53 +0800

    get average Ein

--- prob18.py
+++ prob18.py
@@ -6,14 +6,13 @@ class Data:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    def __repr__(self):  
-                return repr((self.x, self.y))  
+
     def printData(self):
         print(self.x, self.y)
 
-def randMaker():
+def randMaker(n):
     datalist = []
-    for i in range(20):
+    for i in range(n):
         x = random.uniform(-1,1)
         n = random.uniform(0,1) - 0.2
         if x < 0:
@@ -29,10 +28,38 @@ def randMaker():
         data = Data(x, y)
         datalist.append(data)
     return datalist
-#def check(list, theta):
-    #end of check
-datalist = randMaker()
-datalist = sorted(datalist, key=attrgetter('x'))
-for i in range(20):
-    datalist[i].printData()
+
+def check(list):
+    length = len(list)
+    error = 0;
+    bestError = length
+    bestTheta = 0;
+    for i in range(length):
+        if ( list[i].y < 0 ):
+            error = error + 1
+        #if
+    #for
+
+    for i in range(length):
+        #theta moves right, so if yi > 0, 
+        #it will be misclassified
+        error = error + list[i].y
+        if ( error < bestError ):
+            bestError = error
+            bestTheta = i + 1 #theta is placed at the right side of xi
+        #if
+    #for
+    return bestError, bestTheta
+#def check
+totalError = 0;
+for i in range(5000):
+    datalist = randMaker(20)
+    datalist = sorted(datalist, key=attrgetter('x'))
+    #for i in range(20):
+    #    datalist[i].printData()
+    error, theta = check(datalist)
+    totalError = totalError + error
+#for
+
+print(totalError/5000)
 
