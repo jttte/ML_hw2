f91c36e12acda80a94ee5ab8fef442514c0a7e7d
Lucy Lin <Lucy@cardinalblue.com>
2013-10-22 00:28:48 +0800

    prob 18 -20

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ s, i, theta, Ein = MultiDimDS(trainData)
 print(s,i,theta,Ein)
 Eout = CalculateEout(theta, s)
 print(Eout)
+
+data = ParseData(testData, i-1)
+testEout = mDecitionStump(data, s, theta)
+print(testEout)
